16aa4907d1024f2dccdc92275743c62077613580
ogzCode <oguzguc44@gmail.com>
2024-06-25 10:50:44 +0300

    chore: Update App component to include row expansion and actions in data table

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { DataTable } from './data-table/DataTable'
 
-import { columns } from "./data-table/columns"
+import { columns, districtsColumns } from "./data-table/columns"
 
 import axios from 'axios'
+
+import { RowAction } from './data-table/RowAction'
 
 
 function App() {
   const [countryList, setCountryList] = useState([])
   const [total, setTotal] = useState(0)
+  const actions = [
+    {
+      label: "Edit",
+      icon: null,
+      onSelect: (rowData) => console.log(rowData),
+    },
+    {
+      label: "-"
+    },
+    {
+      label: "Delete",
+      icon: null,
+      onSelect: (rowData) => console.log(rowData),
+    },
+  ]
+
+  const loadData = useCallback(({ sorting, columnFilters, pagination }) => {
+
+    axios.get('/api/getAllCountry', {
+      params: {
+        sorting: JSON.stringify(sorting),
+        columnFilters: JSON.stringify(columnFilters),
+        pagination: JSON.stringify(pagination),
+      },
+    }).then(res => {
+      setCountryList(res.data.countries)
+      setTotal(res.data.total)
+    })
+  }, [])
 
   useEffect(() => {
     loadData({
@@ -20,27 +51,32 @@ function App() {
         pageIndex: 0,
       }
     })
-  }, [])
+  }, [loadData])
 
-  const loadData = async ({ sorting, columnFilters, pagination }) => {
-
-    const res = await axios.get('/api/getAllCountry', {
-      params: {
-        sorting: JSON.stringify(sorting),
-        columnFilters: JSON.stringify(columnFilters),
-        pagination: JSON.stringify(pagination),
-      },
-    })
-
-    console.log(res.data.countries, res.data.total)
-
-    setCountryList(res.data.countries)
-    setTotal(res.data.total)
+  const rowExpansionTemplate = ({ row }) => {
+    return (
+      <div>
+        <h1 className='text-xl mb-4'>District</h1>
+        <DataTable
+          columns={districtsColumns}
+          data={row.original.ilceler}
+          total={row.original.ilceler.length}
+          rowAction={(rowData) => <RowAction rowData={rowData} actions={actions} />}
+        />
+      </div>
+    )
   }
 
   return (
     <div className='p-16'>
-      <DataTable columns={columns} data={countryList} fetchFn={loadData} total={total} />
+      <DataTable
+        columns={columns}
+        data={countryList}
+        fetchFn={loadData}
+        total={total}
+        rowExpansionTemplate={rowExpansionTemplate}
+        rowAction={(rowData) => <RowAction rowData={rowData} actions={actions} />}
+      />
     </div>
   )
 }
